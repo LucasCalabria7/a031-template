d476cf271e48bdbed27307a6dc8ed3e03eef68d0
Lucas Calabria <lucascalabria86@gmail.com>
2022-10-11 13:00:41 -0300

    React

---
+++ template/src/components/Footer.js
@@ -0,0 +1,11 @@
+import {Redes} from "./Redes"
+import {Copy} from "./Copy"
+
+export function Footer () {
+    return(
+        <footer>
+            <Redes/>
+            <Copy/>
+        </footer>
+    )
+}
---
+++ template/src/components/Redes.js
@@ -0,0 +1,7 @@
+export function Redes () {
+    return (
+        <div>
+        <p><a href="https://github.com/LucasCalabria7">GitHub</a></p>
+        </div>
+    )
+}
